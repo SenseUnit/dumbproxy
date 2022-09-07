2e2f2c29af16051ef91f1f2d3635140f97e705a6
Vladislav Yarmak <vladislav-ex-src@vm-0.com>
2022-09-07 10:27:33 +0300

    fix cert auth

--- utils.go
+++ utils.go
@@ -149,7 +149,7 @@ func copyBody(wr io.Writer, body io.Reader) {
 	}
 }
 
-func makeServerTLSConfig(certfile, keyfile, cafile string) (*tls.Config, error) {
+func makeServerTLSConfig(certfile, keyfile, cafile, ciphers string) (*tls.Config, error) {
 	var cfg tls.Config
 	cert, err := tls.LoadX509KeyPair(certfile, keyfile)
 	if err != nil {
@@ -168,7 +168,25 @@ func makeServerTLSConfig(certfile, keyfile, cafile string) (*tls.Config, error) 
 		cfg.ClientCAs = roots
 		cfg.ClientAuth = tls.VerifyClientCertIfGiven
 	}
+	cfg.CipherSuites = makeCipherList(ciphers)
 	return &cfg, nil
+}
+
+func updateServerTLSConfig(cfg *tls.Config, cafile, ciphers string) (*tls.Config, error) {
+	if cafile != "" {
+		roots := x509.NewCertPool()
+		certs, err := ioutil.ReadFile(cafile)
+		if err != nil {
+			return nil, err
+		}
+		if ok := roots.AppendCertsFromPEM(certs); !ok {
+			return nil, errors.New("Failed to load CA certificates")
+		}
+		cfg.ClientCAs = roots
+		cfg.ClientAuth = tls.VerifyClientCertIfGiven
+	}
+	cfg.CipherSuites = makeCipherList(ciphers)
+	return cfg, nil
 }
 
 func makeCipherList(ciphers string) []uint16 {
