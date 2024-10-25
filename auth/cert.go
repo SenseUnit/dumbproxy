b2fa525f49e18303086cc94ede0790917a388004
Vladislav Yarmak <vladislav@vm-0.com>
2024-10-25 16:06:54 +0300

    cert auth: improve logging

--- auth/cert.go
+++ auth/cert.go
@@ -1,6 +1,11 @@
 package auth
 
-import "net/http"
+import (
+	"encoding/hex"
+	"fmt"
+	"math/big"
+	"net/http"
+)
 
 type CertAuth struct{}
 
@@ -8,9 +13,24 @@ func (_ CertAuth) Validate(wr http.ResponseWriter, req *http.Request) (string, b
 	if req.TLS == nil || len(req.TLS.VerifiedChains) < 1 || len(req.TLS.VerifiedChains[0]) < 1 {
 		http.Error(wr, BAD_REQ_MSG, http.StatusBadRequest)
 		return "", false
-	} else {
-		return req.TLS.VerifiedChains[0][0].Subject.String(), true
 	}
+	return fmt.Sprintf(
+		"Subject: %s, Serial Number: %s",
+		req.TLS.VerifiedChains[0][0].Subject.String(),
+		formatSerial(req.TLS.VerifiedChains[0][0].SerialNumber),
+	), true
 }
 
 func (_ CertAuth) Stop() {}
+
+// formatSerial from https://codereview.stackexchange.com/a/165708
+func formatSerial(serial *big.Int) string {
+	b := serial.Bytes()
+	buf := make([]byte, 0, 3*len(b))
+	x := buf[1*len(b) : 3*len(b)]
+	hex.Encode(x, b)
+	for i := 0; i < len(x); i += 2 {
+		buf = append(buf, x[i], x[i+1], ':')
+	}
+	return string(buf[:len(buf)-1])
+}
