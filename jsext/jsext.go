a9401f851ff866ee29ff637e17ff6f67e48450e3
Vladislav Yarmak <vladislav@vm-0.com>
2026-06-12 23:46:07 +0300

    js: add shExpMatch

--- jsext/jsext.go
+++ jsext/jsext.go
@@ -19,6 +19,7 @@ func ConfigureRuntime(vm *goja.Runtime) error {
 		AddMMDBReader,
 		ExportEnv,
 		AddConvertAddr,
+		AddShExpMatch,
 	} {
 		if err := f(vm); err != nil {
 			return fmt.Errorf("JS runtime init part #%d failed: %w", idx+1, err)
